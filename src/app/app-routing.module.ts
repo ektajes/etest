eb331a6beec105b0e0efd897135f3c29189eda54
ekta <ekts=a4994@gmail.com>
2021-11-15 03:17:37 +0530

    ;ic'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
-
+  {path:'', redirectTo:'/product', pathMatch:'full'},
+{path:'product', component:ProductComponent},
   {path:'cart', component:CartComponent}
 ];
 
